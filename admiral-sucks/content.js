e7afac02d2002d44a0a2eb30a2f3fb455f7569c9
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2021-01-11 22:55:27 -0500

    v1.1 with some more hardcore logic

--- admiral-sucks/content.js
+++ admiral-sucks/content.js
@@ -6,10 +6,37 @@ var tags = htmls.concat(bodys, divs);
 
 for (var i=0; i < tags.length; i+= 1) {
     var t = tags[i];
-    if (t.style.zIndex > 100) {
+    var cs = window.getComputedStyle(t);
+    if (t.style.zIndex > 100 || cs.zIndex > 100) {
         t.style = "";
         t.style.display = "none";
     }
+    
     t.style.position = "";
-    t.style.overflow = "";
+
+    if (cs.overflow == "hidden") 
+        t.style.overflow = "scroll";
+}
+
+
+// for hard-core scroll blockers
+
+for(var i = 0; i < document.styleSheets.length; i++) {
+    var classes = null;
+    try {
+        classes = document.styleSheets[i].rules || document.styleSheets[i].cssRules;
+    } catch (e) {
+        console.warn("Can't read the css rules of: " + document.styleSheets[i].href, e);
+        continue;
+    }
+    for(var j = 0; j < document.styleSheets[i].rules.length; j++) {
+        if (document.styleSheets[i].rules[j].style) {
+            if (document.styleSheets[i].rules[j].style.overflow == "hidden") {
+                var selectorText = document.styleSheets[i].rules[j].selectorText;
+                //console.log(i + " " + j);
+                //console.log(document.styleSheets[i].rules[j].selectorText);
+                document.styleSheets[i].rules[j].style.overflow = "scroll";
+            }
+        }
+    }
 }
